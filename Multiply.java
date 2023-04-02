1fddaf26f8a66f6684c257aa5457172fd6c963e7
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-04-02 03:01:00 -0700

    Add class skeleton

---
+++ Multiply.java
@@ -0,0 +1,34 @@
+// CS 3310.01 Spring 2023
+// Janista Gitbumrungsin
+// Project 1 Matrix Multiplication
+
+public class Multiply
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    public static int[][] generateMatrix(int[][] matrix)
+    {
+        return matrix;
+    }
+
+    public static int[][] classical(int[][] A, int[][] B, int n)
+    {
+        int[][] C = new int[n][n];
+        return C;
+    }
+
+    public static int[][] divideAndConquer(int[][] A, int[][] B, int n)
+    {
+        int[][] C = new int[n][n];
+        return C;
+    }
+
+    public static int[][] strassen(int[][] A, int[][] B, int n)
+    {
+        int[][] C = new int[n][n];
+        return C;
+    }
+}
