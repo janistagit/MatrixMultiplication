a4745901f2220957bf633d570853d6e63384b4c2
Janista Gitbumrungsin <79038106+janistagit@users.noreply.github.com>
2023-04-02 22:27:50 -0700

    Implement printing matrix to terminal

--- Multiply.java
+++ Multiply.java
@@ -45,6 +45,18 @@ public class Multiply
         return matrix;
     }
 
+    public static void printMatrix(int[][] matrix)
+    {
+        for(int i = 0; i < matrix.length; i++)
+        {
+            for(int j = 0; j < matrix.length; j++)
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static int[][] classical(int[][] A, int[][] B, int n)
     {
         int[][] C = new int[n][n];
